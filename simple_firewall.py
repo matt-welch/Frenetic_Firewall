a94c667c0486fd41651aed48a7395f22626927a4
Matt Welch <mattw0308@gmail.com>
2014-04-17 11:01:07 -0700

    Figured out IP wildcards

--- simple_firewall.py
+++ simple_firewall.py
@@ -9,6 +9,7 @@ from pox.lib.addresses import *
 from pyretic.lib.corelib import *
 from pyretic.lib.std import *
 from pyretic.modules.mac_learner import mac_learner
+import pox.lib.packet as pkt
 
 class firewall(DynamicPolicy):
 
@@ -17,8 +18,8 @@ class firewall(DynamicPolicy):
         print "initializing firewall"
         self.firewall = {}
         super(firewall,self).__init__(true)
-        self.AddRule('10.0.0.1',5555,'10.0.0.2',4444)
-
+		# 0.0.0.0/0 is the wildcard for the ANY IP address match
+        self.AddRule('0.0.0.0/0',5555,'10.0.0.2',555)
     def AddRule(self, ip1, port1, ip2, port2):
         self.firewall[(ip1, port1, ip2, port2)] = True
         print "Adding Firewall rule in %s:%s -  %s:%s" % (ip1 , port1 , ip2 , port2)
@@ -26,7 +27,14 @@ class firewall(DynamicPolicy):
 
     def update_policy(self):
         # select allowed traffic
-        self.policy = union([ match(srcip=ip1) & match(dstip=ip2)
+		#
+		#
+		#if(ip1=='any'): ip1='0.0.0.0/0'
+
+        self.policy = union([ match(srcip=ip1) & match(dstip=ip2) &
+                match(protocol=pkt.ipv4.TCP_PROTOCOL ) & 
+				match(dstport=port2) & 
+                match(ethtype=pkt.ethernet.IP_TYPE)
                 for (ip1, port1, ip2, port2)
                 in self.firewall.keys() ])
         print self.policy
@@ -35,4 +43,5 @@ def main(configuration=""):
     # read config file
     # for everline in config file
     # call AddRule(dstip, dstport, srcport, srcport)
-    return firewall() >> flood()
+    return firewall() >> fwd(2)
+    # was flood()
