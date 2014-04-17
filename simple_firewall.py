621ad5c22e90ad6b749dcd6ab4ea37102a26aa55
Matt Welch <mattw0308@gmail.com>
2014-04-17 11:31:35 -0700

    Adding new policy in a for loop and handling wildcards

--- simple_firewall.py
+++ simple_firewall.py
@@ -19,11 +19,11 @@ class firewall(DynamicPolicy):
         self.firewall = {}
         super(firewall,self).__init__(true)
 		# 0.0.0.0/0 is the wildcard for the ANY IP address match
-        self.AddRule('0.0.0.0/0',5555,'10.0.0.2',555)
+        self.AddRule('10.0.0.1',5555,'10.0.0.2',555)
+        self.update_policy()
     def AddRule(self, ip1, port1, ip2, port2):
         self.firewall[(ip1, port1, ip2, port2)] = True
         print "Adding Firewall rule in %s:%s -  %s:%s" % (ip1 , port1 , ip2 , port2)
-        self.update_policy()
 
     def update_policy(self):
         # select allowed traffic
@@ -31,12 +31,24 @@ class firewall(DynamicPolicy):
 		#
 		#if(ip1=='any'): ip1='0.0.0.0/0'
 
-        self.policy = union([ match(srcip=ip1) & match(dstip=ip2) &
-                match(protocol=pkt.ipv4.TCP_PROTOCOL ) & 
-				match(dstport=port2) & 
-                match(ethtype=pkt.ethernet.IP_TYPE)
-                for (ip1, port1, ip2, port2)
-                in self.firewall.keys() ])
+        protomatch = match(protocol=pkt.ipv4.TCP_PROTOCOL ) & match(dstport=port2) &
+            match(ethtype=pkt.ethernet.IP_TYPE)
+        for (ip1, port1, ip2, port2) in self.firewall.keys() 
+            # new policy is initially a basic policy matching only on protocols
+            newpolicy = protomatch
+
+            # build a new policy
+            if(ip1 != 'any'):
+                newpolicy = newpolicy & match(srcip=ip1)
+            if(srcport != 'any'):
+                newpolicy = newpolicy & match(srcport=port1)
+            if(ip2 != 'any'):
+                newpolicy = newpolicy & match(dstip=ip2)
+            if(dstport != 'any'):
+                newpolicy = newpolicy & match(dstport=port2)
+    
+            # join with the old policy
+            self.policy = union([ newpolicy & policy ])
         print self.policy
 
 def main(configuration=""):
