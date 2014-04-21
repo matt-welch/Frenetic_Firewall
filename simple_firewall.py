c83b89d499cfd29041fbe57ba6b9714871c859fc
Matt Welch <mattw0308@gmail.com>
2014-04-21 16:08:27 -0700

    Forgot to add changes to simple firewall to the last commit

--- simple_firewall.py
+++ simple_firewall.py
@@ -1,7 +1,7 @@
 """
 Team 5 - Ben Boren, Erin Lanus, Matt Welch
 simple_firewall.py reimplements the firewall-like functionality in the 
-previous Firewall.py POX module. This is a reimplementation in Pyretic .
+previous Firewall.py POX module. This is a reimplementation in Pyretic.
 """
 
 from pox.lib.addresses import *
@@ -18,24 +18,59 @@ class firewall(DynamicPolicy):
         print "initializing firewall"
         self.firewall = {}
         super(firewall,self).__init__(true)
-		# 0.0.0.0/0 is the wildcard for the ANY IP address match
-        self.AddRule('10.0.0.1',1111,'10.0.0.2',2222)
-        self.AddRule('10.0.0.3',3333,'10.0.0.4',4444)
-        self.AddRule('10.0.0.5',5555,'any','any')
-        self.AddRule('10.0.0.5','any','any','any')
 
+        # push rules from the configuration file
+        global config
+        fin = open(config)
+        for line in fin:
+            rule = line.split()
+            print rule
+            if rule[1] != 'any':
+                rule[1] = int(rule[1])
+            if rule[3] != 'any':
+                rule[3] = int(rule[3])
+            if (len(rule) > 0) : # only make a rule if the line is not empty
+                self.AddRule(rule[0], rule[1], rule[2], rule[3])
+                print "Adding rule for: <",rule,">"
+                #config.append(rule)
+            if (False):
+                print config   # for everline in config file
+    
+        # 0.0.0.0/0 is the wildcard for the ANY IP address match
+        # can add rules hardcoded 
+#        self.AddRule('10.0.0.5',5555,'any','any')
+        print self.firewall
         self.update_policy()
+
+        
     def AddRule(self, ip1, port1, ip2, port2):
         self.firewall[(ip1, port1, ip2, port2)] = True
         print "Adding Firewall rule in %s:%s -  %s:%s" % (ip1 , port1 , ip2 , port2)
 
+    def clean_ip (cidrAddress):
+        """
+        Takes an address if the address is in CIDR notation and contains uintAddress
+        hostAddress then the netmask portion is stripped from the address so that the
+        address may be installed as an IP address in uintAddress flow
+        (e.g. 192.168.1.4/24 becomes 192.168.1.4)
+        """
+        if PRINT_FUNCTION_NAMES:
+            print "clean_ip()"
+        strAddress = cidrAddress.split('/', 2)
+        if len(strAddress) == 1:
+            return cidrAddress
+        uintAddress = IPAddr(strAddress[0]).toUnsigned()
+        hostMask = 32-int(strAddress[1])
+        hostAddress = uintAddress & ( (1<<hostMask) - 1 )
+        if (hostAddress == 0):
+            return cidrAddress
+        else:
+            return strAddress[0]
+    
     def update_policy(self):
-        # select allowed traffic
-		#
-		#
-		#if(ip1=='any'): ip1='0.0.0.0/0'
-
+        # create a prototype rule specifying the ethertype and TCP
         protomatch = match(protocol=pkt.ipv4.TCP_PROTOCOL ) & match(ethtype=pkt.ethernet.IP_TYPE)
+        # loop through the rules in the dictionary, unioning them with the existing policy
         for (ip1, port1, ip2, port2) in self.firewall.keys(): 
             # new policy is initially a basic policy matching only on protocols
             newpolicy = protomatch
@@ -49,14 +84,24 @@ class firewall(DynamicPolicy):
                 newpolicy = newpolicy & match(dstip=ip2)
             if(port2 != 'any'):
                 newpolicy = newpolicy & match(dstport=port2)
-    
+
             # join with the old policy
             self.policy = union([ newpolicy + self.policy ])
+
+        # add rules to the policy to explicitly allow ICMP and ARP traffic to passthrough
+        self.policy =  union([(match(protocol=pkt.ipv4.ICMP_PROTOCOL, ethtype=pkt.ethernet.IP_TYPE)) + 
+            (match(protocol=pkt.arp.REQUEST,     ethtype=pkt.ethernet.ARP_TYPE)) +
+            (match(protocol=pkt.arp.REPLY,       ethtype=pkt.ethernet.ARP_TYPE)) +
+            (match(protocol=pkt.arp.REV_REQUEST, ethtype=pkt.ethernet.ARP_TYPE)) +
+            (match(protocol=pkt.arp.REV_REPLY,   ethtype=pkt.ethernet.ARP_TYPE)) +
+            self.policy ])   
         print self.policy
 
 def main(configuration=""):
     # read config file
-    # for everline in config file
+    print configuration
+    global config
+    config=configuration
     # call AddRule(dstip, dstport, srcport, srcport)
-    return firewall() >> fwd(2)
+    return firewall() >> fwd(2) #mac_learner()
     # was flood()
